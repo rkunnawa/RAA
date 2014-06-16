718d54893194ac8d2e94bdd65dbde7df9bcb632e
Raghav Kunnawalkam Elayavalli <raghav.k.e@cern.ch>
2014-06-16 09:48:12 +0200

    created the plots macro

---
+++ RAA_plot.C
@@ -0,0 +1,23 @@
+// Raghav Kunnawalkam Elayavalli
+// June 11 2014
+// CERN
+
+// RAA - plotting macro. pretty much all the neccessary plots will be made here in this macro. 
+// will take input from the read macro and the analysis macro. Maybe not from the read macro - i will have to decide on that. 
+
+// 
+
+#include <iostream>
+#include <stdio.h>
+#include <fstream>
+
+
+
+
+void RAA_plot(){
+
+
+
+
+
+}
