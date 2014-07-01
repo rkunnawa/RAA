ffacbdb12cf4fc4aebbd8761e0a6c3e7b0cb47b6
Raghav Kunnawalkam Elayavalli <raghav.k.e@cern.ch>
2014-07-01 15:35:59 +0200

    added separate plot headers and first commit of the nlo comparison macro

---
+++ Headers/SmearingFactors.h
@@ -0,0 +1,3 @@
+// file taken from github.com/pawannetrakanti/pawan/data/smearingfactors.h
+// maybe its required for the removing duplicate events but not sure. could also be useful for the smearing statistics in raa calculation. 
+
---
+++ RAA_nlo_compare.C
@@ -0,0 +1,61 @@
+// Raghav Kunnawalkam Elayavalli
+// July 1st 2014
+// CERN
+
+//
+// Macro to readin the NLO files and compare it with the pp data we have. 
+//
+
+// I need to decide if this macro will also plot the reuqired histograms or would i have to create a new macro for that
+// going by what ive done before i think ill create a new macro. 
+
+
+#include <iostream>
+#include <stdio.h>
+#include <fstream>
+#include <fstream>
+#include <TH1F.h>
+#include <TH1F.h>
+#include <TH2F.h>
+#include <TFile.h>
+#include <TTree.h>
+#include <TF1.h>
+#include <TCanvas.h>
+#include <TLegend.h>
+#include <TGraphErrors.h>
+#include <TGraphAsymmErrors.h>
+#include <TH1.h>
+#include <TH2.h>
+#include <TH3.h>
+#include <TFile.h>
+#include <TStyle.h>
+#include <TStopwatch.h>
+#include <TRandom3.h>
+#include <TChain.h>
+#include <TProfile.h>
+#include <TStopwatch.h>
+#include <TCut.h>
+#include <cstdlib>
+#include <cmath>
+#include "TLegend.h"
+#include "TLatex.h"
+#include "TMath.h"
+#include "TLine.h"
+
+
+  
+static const int nbins_pt = 29;
+static const double boundaries_pt[nbins_pt+1] = {22, 27, 33, 39, 47, 55, 64, 74, 84, 97, 114, 133, 153, 174, 196, 220, 245, 272, 300, 330, 362, 395, 430, 468, 507, 548, 592, 638, 790, 967};
+
+
+using namespace std;
+
+
+void RAA_nlo_compare(int radius = 3){
+
+
+
+
+
+
+}
