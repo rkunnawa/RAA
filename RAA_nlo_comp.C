3d87e17d2eeb3300c019ac587ee6cc0bd4261c75
Raghav Kunnawalkam Elayavalli <raghav.k.e@NOTSPAMcern.ch>
2014-07-11 11:47:45 +0200

    removed the duplicate RAA_nlo_comp.C macro

--- RAA_nlo_comp.C
+++
@@ -1,9 +0,0 @@
-// Raghav Kunnawalkam Elayavalli
-// July 11 2014
-// CERN
-
-//
-// NLO comparison macro to read in nlo files from CMS official FastNLO and compare with the pp data we have at 2.76 TeV. 
-// Now since we have the NP corrections as well, its time to add them to the NLO and check with the data. they all should have the same pt and eta binning. 
-// make the plots to each eta bin as well. 
-
