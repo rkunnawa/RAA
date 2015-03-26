5c88de0fc4225578afc7af0d47b2d2af44ee5cc4
Raghav Kunnawalkam Elayavalli <raghavke@cern.ch>
2015-03-26 11:08:07 -0400

    adding jet id variables check plot

--- RAA_plot_jetidvariables.C
+++ RAA_plot_jetidvariables.C
@@ -2,6 +2,7 @@
 
   TH1::SetDefaultSumw2();
   gSystem->Load("Headers/plot.h");
+  gStyle->SetOptStat(0);
   
   TFile * fData = TFile::Open("../../Output/PbPb_Data_calo_pf_jet_correlation_deltaR_0p2_akPu3_20150320.root");
   TTree * Data_matched = (TTree*)fData->Get("matchedJets");
@@ -20,24 +21,39 @@
   TH1F * hMC_chMaxJtpt[nbins_cent], * hMC_phMaxJtpt[nbins_cent], * hMC_neMaxJtpt[nbins_cent], * hMC_muMaxJtpt[nbins_cent], * hMC_eMaxJtpt[nbins_cent];
   TH1F * hMC_chSumJtpt[nbins_cent], * hMC_phSumJtpt[nbins_cent], * hMC_neSumJtpt[nbins_cent], * hMC_muSumJtpt[nbins_cent], * hMC_eSumJtpt[nbins_cent];  
 
-  for(int i = 0 ; i < 1 ; ++i){
+  TCanvas * cchMaxJtpt[nbins_cent];
+  
+  for(int i = 0 ; i < nbins_cent ; ++i){
 
     hData_chMaxJtpt[i] = new TH1F(Form("hData_chMaxJtpt_cent%d",i),Form("Data chMax/jtpt in centrality bin %d",i),100,0,1);
     hMC_chMaxJtpt[i] = new TH1F(Form("hMC_chMaxJtpt_cent%d",i),Form("MC chMax/jtpt in centrality bin ",i),100,0,1);
     
-    Data_matched->Draw(Form("chMax/pfpt>>hData_chMaxJtpt_cent%d",i),Form("hiBin > %d && hiBin < %d",5 * boundaries_cent[i], 5 * boundaries_cent[i+1]),"");
-    MC_matched->Draw(Form("chMax/pfpt>>hMC_chMaxJtpt_cent%d",i),Form("(hiBin > %d && hiBin < %d)",5 * boundaries_cent[i], 5 * boundaries_cent[i+1]),"");
-  }
+    Data_matched->Draw(Form("chMax/pfpt>>hData_chMaxJtpt_cent%d",i),Form(""),"");
+    MC_matched->Draw(Form("chMax/pfpt>>hMC_chMaxJtpt_cent%d",i),"weight*pfpt","");
+    
+    
+    
+    cchMaxJtpt[i] = new TCanvas(Form("cchMaxJtpt_cent%d",i),"",800,600);
+    cchMaxJtpt[i]->SetLogy();
+    hMC_chMaxJtpt[i]->SetMarkerColor(kRed);
+    hMC_chMaxJtpt[i]->SetMarkerStyle(25);
+    hMC_chMaxJtpt[i]->Print("base");
+    hMC_chMaxJtpt[i]->SetTitle(" ");
+    hMC_chMaxJtpt[i]->Draw();
 
-  TCanvas * cchMaxJtpt = new TCanvas("cchMaxJtpt","",800,600);
-  
-  hData_chMaxJtpt[0]->SetMarkerStyle(24);
-  hData_chMaxJtpt[0]->SetMarkerColor(kBlack);
-  hData_chMaxJtpt[0]->DrawNormalized();
-  hMC_chMaxJtpt[0]->SetMarkerColor(kRed);
-  hMC_chMaxJtpt[0]->SetMarkerStyle(25);
-  hMC_chMaxJtpt[0]->DrawNormalized("same");
-  hMC_chMaxJtpt[0]->Print("base");
-  cchMaxJtpt->SaveAs("","RECREATE");
+    hData_chMaxJtpt[i]->SetMarkerStyle(24);
+    hData_chMaxJtpt[i]->SetMarkerColor(kBlack);
+    hData_chMaxJtpt[i]->DrawNormalized("same");
+
+    TLegend * leg = myLegend(0.15,0.15,0.3,0.3);
+    leg->AddEntry(hMC_chMaxJtpt[1],"MC","pl");
+    leg->AddEntry(hData_chMaxJtpt[1],"Data","pl");
+    leg->SetTextSize(0.04);
+    leg->Draw();
+    drawText(Form("%2.0f - %2.0f % ", 2.5 * boundaries_cent[i], 2.5 * boundaries_cent[i+1]), 0.2,0.4,16);
+    
+    cchMaxJtpt[i]->SaveAs(Form("chMaxJtpt_cent%d.pdf",i),"RECREATE");
+
+  }
 
 }
